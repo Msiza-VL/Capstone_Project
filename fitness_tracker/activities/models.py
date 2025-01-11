a227cd030e3078808c4034c160694b8ac9a3650c
Msiza-VL <lindokehlemsiza.00@gmail.com>
2025-01-11 16:32:13 +0200

    Initial project setup

--- fitness_tracker/activities/models.py
+++ fitness_tracker/activities/models.py
@@ -1,36 +1,3 @@
 from django.db import models
 
-from django.contrib.auth.models import User
-
-
-class Activity(models.Model):
-
-    ACTIVITY_TYPES = [
-
-        ('Running', 'Running'),
-
-        ('Cycling', 'Cycling'),
-
-        ('Weightlifting', 'Weightlifting'),
-
-        # Add more activity types as needed
-
-    ]
-
-
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    activity_type = models.CharField(max_length=50, choices=ACTIVITY_TYPES)
-
-    duration = models.PositiveIntegerField()  # in minutes
-
-    distance = models.FloatField()  # in km or miles
-
-    calories_burned = models.PositiveIntegerField()
-
-    date = models.DateField()
-
-
-    def __str__(self):
-
-        return f"{self.activity_type} by {self.user.username} on {self.date}"
+# Create your models here.
